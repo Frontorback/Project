efdc66677803bf1c692ae4216633fa353ebf667f
Frontorback <artemkravzovv@gmail.com>
2020-12-18 21:55:34 +0200

    completed

---
+++ Submit-massage.js
@@ -0,0 +1,17 @@
+const SubmitBtn = document.querySelector('.submit-button');
+const NameTxt = document.querySelector('.contact-second-text');
+
+NameTxt.innerText = '';
+
+function afafs (sf){
+    sf.NameTxt.innerText = '';
+}
+(function(){
+
+this.NameTxt.addEventListener( ' click', sf());
+
+
+    
+
+}
+)();
